8c7ec1e0fafa4f29e90a3f7ff0169c12a16b8c1b
Ryan <marandoryan@gmail.com>
2024-05-14 00:12:27 -0400

    change middlware to js

--- weather-alert-system-app/src/components/Navbar.js
+++ weather-alert-system-app/src/components/Navbar.js
@@ -48,7 +48,7 @@ export function Navbar() {
 
         <div className="flex items-center justify-center pr-4">
           <SignedOut>
-            <Link href="/sign-in">Signin</Link>
+            <Link href="/sign-in">Sign In</Link>
           </SignedOut>
           <SignedIn>
             <UserButton />
