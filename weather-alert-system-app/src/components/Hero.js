9a99e547d21628e1e2e663c904f997f72a3d2b93
Ryan Marando <marandoryan@gmail.com>
2024-09-27 11:17:53 -0400

    fixed bug with apostrophe

--- weather-alert-system-app/src/components/Hero.js
+++ weather-alert-system-app/src/components/Hero.js
@@ -99,14 +99,25 @@ export function Hero() {
       <div className="absolute flex mt-60 mr-[50%]">
         <YouTubeEmbed url={"https://youtu.be/g8IAGWvhe-s"} autoplay={true}/>
       </div>
-      <div className="absolute flex ml-[45%] mt-48 items-center">
+      <div className="absolute flex ml-[43%] mt-48 items-center">
         <h2 className="font-semibold text-[20px] text-center">About Ryan Marando</h2>
       </div>
       <div className="absolute flex ml-[45%] mt-60 w-[50%] text-center">
-        <p className="border-2 border-gray-800 rounded-md p-2">Colleagues call me an engaging and compelling meteorologist who blends storytelling and science with ease. As a Meteorologist for WHIO in Dayton, Ohio, <strong>I shape my forecasts to help the community understand the impacts of everyday to dangerous, severe weather.</strong></p>
-        <p className="border-2 border-gray-800 rounded-md p-2 ml-2">What I enjoy most is serving my community by producing accurate, digestible, and refreshing forecasts using creativity with augmented 3D graphics and enhancing them with my Python certification and programming experience. <strong>To me, it's important to not only tell the weather story but also the context and perspective behind it has so viewers truly understand the forecast.</strong></p>
-        <p className="border-2 border-gray-800 rounded-md p-2 ml-2">Weather is my passion! <strong>My constant curiosity about our world and the impact science has started when I was young growing up in Bear, Delaware.</strong> That love fueled my education graduating Summa Cum Laude as Embry-Riddle Aeronautical University's Meteorology Student of the Year.</p>
-      </div>
+      <p className="border-2 border-gray-800 rounded-md p-2">
+  Colleagues call me an engaging and compelling meteorologist who blends storytelling and science with ease. 
+  As a Meteorologist for WHIO in Dayton, Ohio, <strong>I shape my forecasts to help the community understand the impacts of everyday to dangerous, severe weather.</strong>
+</p>
+<p className="border-2 border-gray-800 rounded-md p-2 ml-2">
+  What I enjoy most is serving my community by producing accurate, digestible, and refreshing forecasts using creativity with augmented 3D graphics 
+  and enhancing them with my Python certification and programming experience. 
+  <strong> To me, it&apos;s important to not only tell the weather story but also the context and perspective behind it so viewers truly understand the forecast.</strong>
+</p>
+<p className="border-2 border-gray-800 rounded-md p-2 ml-2">
+  Weather is my passion! 
+  <strong> My constant curiosity about our world and the impact science has started when I was young growing up in Bear, Delaware.</strong> 
+  That love fueled my education graduating Summa Cum Laude as Embry-Riddle Aeronautical University&apos;s Meteorology Student of the Year.
+</p>
+</div>
         <Image
           src={Gradient}
           alt="Gradient"
