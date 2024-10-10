7421823a8d0b98f23f1866e3b3362b6df131ff3e
Ryan Marando <marandoryan@gmail.com>
2024-10-10 08:43:18 -0400

    full broadcasting and update winter alerts

--- weather-alert-system-app/src/app/broadcasting/page.js
+++ weather-alert-system-app/src/app/broadcasting/page.js
@@ -50,11 +50,68 @@ export default function Broadcasting() {
 
       const Graphics = () => {
         return (
-          <div>
-            <h1 className="ml-2">Broadcasting Graphics</h1>
+          <div className="p-4">
+            <div className="flex items-center">
+            <h1 className="sm: text-md ml-2 lg:text-2xl font-bold mb-6 text-center">Graphics Powered With Programming</h1>
+            <p className="text-sm text-center w-[70%] mb-8 lg:text-base">
+              These graphics use more than the every day graphics system of the Max system or Baron Lynx.
+              Using computer programming from python to automate graphics for social media and on air use.
+            </p>
+            </div>
+            {/* Top row: 3 images with titles */}
+            <div className="lg:grid lg:grid-cols-3 lg:gap-4">
+              <div className="text-center">
+                <h2 className="font-medium text-lg">Alert System</h2>
+                <img
+                  src="/assets/alert_system_coded.png"
+                  alt="Graphic 1"
+                  className="w-full object-cover"
+                />
+              </div>
+              <div className="text-center">
+                <h2 className="font-medium text-lg">Hurricane Forecast Ingredients</h2>
+                <img
+                  src="/assets/hurricane_coded.png"
+                  alt="Graphic 2"
+                  className="w-full object-cover"
+                />
+              </div>
+              <div className="text-center">
+                <h2 className="font-medium text-lg">Rainfall Totals</h2>
+                <img
+                  src="/assets/rainfall_coded.png"
+                  alt="Graphic 3"
+                  className="w-full object-cover"
+                />
+              </div>
+            </div>
+      
+            {/* Second row: 2 images centered horizontally */}
+            <div className="lg:flex lg:justify-center lg:gap-4 lg:mt-4">
+              <div className="text-center">
+                <h2 className="font-medium text-lg">Temperature Readings</h2>
+                <img
+                  src="/assets/temps_coded.png"
+                  alt="Graphic 4"
+                  className="w-full h-60 object-cover"
+                />
+              </div>
+              <div className="text-center">
+                <h2 className="font-medium text-lg">Wet Bulb Globe Temperature Forecasts</h2>
+                <img
+                  src="/assets/wbgt_coded.png"
+                  alt="Graphic 5"
+                  className="w-full h-60 object-cover"
+                />
+              </div>
+            </div>
           </div>
-        )
-      }
+        );
+      };
+
+      
+      
+      
 
     return (
         <div>
